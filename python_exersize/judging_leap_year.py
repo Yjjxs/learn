35a4627d9d4c349f7daf2751d26045c87934568f
kathey <1730064247@qq.com>
2020-11-25 13:50:34 +0800

    Add some notes.

--- python_exersize/judging_leap_year.py
+++ python_exersize/judging_leap_year.py
@@ -3,6 +3,7 @@ year = int(input('请输入年份：'))
 if (year % 4) == 0:
     if (year % 100) == 0:
         if (year % 400) == 0:
+		    #f-string 格式化 是在字符串模板前面加上f，然后占位符使用{} ,里面直接放入对应的数据对象。
             print(f'{year} is leap year')
         else:
             print(f'{year} is not leap year') 
