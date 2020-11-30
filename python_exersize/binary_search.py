a54dd23cf600153b271697332dffbaffa840b57a
kathey <1730064247@qq.com>
2020-11-30 17:50:11 +0800

    update the code of binarysearch .

--- python_exersize/binary_search.py
+++ python_exersize/binary_search.py
@@ -1,40 +1,41 @@
 #递归处理（还有点问题）
-from bubblesort1 import bubbleSort
+from bubblesort import bubbleSort
 
-def binerySearch(arr,a,r,x):
-    #元素多于一个
-    if r:
-        mid = int(1+(r-1)/2)
-    
-        #元素正好再中间
-        if arr[mid] == x:
-            return mid
-
-        #小于中间位置数字
-        elif arr[mid] > x:
-            return binerySearch(arr,a,mid-1,x)
-        
-        else:
-            return binerySearch(arr,a,mid+1,x)
-    
-    #不存在
-    else:
+def binarySearch (arr, l, r, x): 
+  
+    # 基本判断
+    if r >= l: 
+  
+        mid = int(l + (r - l)/2)
+  
+        # 元素整好的中间位置
+        if arr[mid] == x: 
+            return mid 
+          
+        # 元素小于中间位置的元素，只需要再比较左边的元素
+        elif arr[mid] > x: 
+            return binarySearch(arr, l, mid-1, x) 
+  
+        # 元素大于中间位置的元素，只需要再比较右边的元素
+        else: 
+            return binarySearch(arr, mid+1, r, x) 
+  
+    else: 
+        # 不存在
         return -1
-
+  
 arr = [3,1,55,43,28,67,79,346,361,234,43,33,456,3435]
 
 bubbleSort(arr)
 for i in range(len(arr)):
     print("%d"%arr[i])
 
-x = int(input("请输入要找的数字："))
-#x=4
-
-res = binerySearch(arr,0,len(arr)-1,x)
-
-if res != -1:
-    print ("元素在数组中的索引为 %d" % res )
-else:
-    print("元素不在数组中")
-
-
+x  = int(input("请输入要找的数字："))
+ 
+# 函数调用
+result = binarySearch(arr, 0, len(arr)-1, x) 
+  
+if result != -1: 
+    print ("元素在数组中的索引为 %d" % result )
+else: 
+    print ("元素不在数组中")
